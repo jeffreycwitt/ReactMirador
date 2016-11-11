606395a02cba6a49932e32c35c5f7193ce9f86ef
Jeffrey C. Witt <jeffreycwitt@gmail.com>
2016-11-11 09:45:39 -0500

    temporary fix to limit expressionInfo only to structureCollection levels

--- app/components/ExpressionInfo.jsx
+++ app/components/ExpressionInfo.jsx
@@ -4,11 +4,15 @@ var {Link, IndexLink} = require("react-router");
 
 var expressionInfoQuery = function(expressionid){
 	var query = [
-			"SELECT ?expression_title ?expression_child WHERE {",
+			"SELECT ?expression_title ?expression_child ?structureType ?child_structureType WHERE {",
 			"<http://scta.info/resource/" + expressionid + "> ",
 			"<http://purl.org/dc/elements/1.1/title> ?expression_title .",
+			"<http://scta.info/resource/" + expressionid + "> ",
+			"<http://scta.info/property/structureType> ?structureType .",
+
 			"OPTIONAL {",
 				"<http://scta.info/resource/" + expressionid + "> <http://purl.org/dc/terms/hasPart>  ?expression_child .",
+				"?expression_child <http://scta.info/property/structureType> ?child_structureType .",
 				"}",
 			"}"
 		].join('');
@@ -27,21 +31,28 @@ var ExpressionInfo = React.createClass({
 		//clear out old state
 		_this.setState({
 			expressionTitle: undefined,
-			expressionChildren: undefined
+			expressionChildren: undefined,
+			expressionStructureType: undefined
 		});
 
 		sctaData.getData(query).then(function(resp){
 
 			var expression_title = resp.results.bindings[0].expression_title.value
+			var expressionStructureType = resp.results.bindings[0].structureType.value
 			var expression_children = []
 			resp.results.bindings.forEach(function(result){
 				var expression_child = result.expression_child.value;
 				var child_shortid = expression_child.split("/").pop(-1);
-				expression_children.push(<li key={child_shortid}><Link to={child_shortid}>{expression_child}</Link></li>);
-				});
+				var child_structureType = result.child_structureType.value;
+				if (child_structureType === "http://scta.info/resource/structureCollection"){
+					expression_children.push(<li key={child_shortid}><Link to={child_shortid}>{expression_child}</Link></li>);
+				}
+
+			});
 			_this.setState({
 				expressionChildren: expression_children,
-				expressionTitle: expression_title
+				expressionTitle: expression_title,
+				expressionStructureType: expressionStructureType
 			});
 		});
 	},
